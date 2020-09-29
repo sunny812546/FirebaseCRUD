f8327fa7e06d6dba2daf5a6bf44da215b82c2ef1
sunny812546 <sun812546@gmail.com>
2020-09-29 17:21:30 +0530

    first commit

---
+++ src/utils/config.js
@@ -0,0 +1,19 @@
+//TODO: DONE add firebase configuration and export it
+export const firebaseConfig = {
+  //TODO: ADD YOUR FIREBASE INFO HERE
+  apiKey: "AIzaSyBnIjbFCqrz8it9UhrVbsa0fitsPpTOotE",
+  authDomain: "mygitapp-3d46f.firebaseapp.com",
+  databaseURL: "https://mygitapp-3d46f.firebaseio.com",
+  projectId: "mygitapp-3d46f",
+  storageBucket: "mygitapp-3d46f.appspot.com",
+  messagingSenderId: "510683951954",
+  appId: "1:510683951954:web:2879c74b5a65f24dbec08b",
+  measurementId: "G-Y6WFLX8C8H",
+};
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true,
+};
